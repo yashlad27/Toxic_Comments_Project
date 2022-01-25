700939e165e8f58e20ceb0a4c01ebcf0f9bc06f6
silentknight <chitnisapoorv@gmail.com>
2022-01-25 19:54:21 +0530

    stemmer

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.snowball import SnowballStemmer
 
 import re
 import string
 
 stop = stopwords.words('english')
 df = pd.read_csv("dataset/train.csv")
+stemmer = SnowballStemmer('english')
+
+#remove alphanumeric
 alphanumeric = lambda x: re.sub('\w*\d\w*', ' ', x)
 
 # '[%s]' % re.escape(string.punctuation),' ' - replace punctuation with white space
@@ -28,17 +30,22 @@ df['comment_text'] = df['comment_text'].map(alphanumeric).map(punc_lower).map(re
 
 #stopwords
 df["comment_text"] = df["comment_text"].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+
+#base form
+df['comment_text'].apply(lambda x: [stemmer.stem(y) for y in x])
 print(df["comment_text"])
 
-df_test = pd.read_csv("dataset/test.csv")
-X_train = np.array(df["comment_text"])
-# print(df_test.head())
-#print(x_train.shape)
-y_train = np.array(df[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]])
-X_test = np.array(df_test["comment_text"])
-# print(x_test)
-# print(y_train)
-l_reg = linear_model.LinearRegression()
-model = l_reg.fit(X_train, y_train)
-predictions = model.predict(X_test)
-print("predictions: ", predictions) 
+#**********idhar se aage karna hai, label encoding baaki hai sirf, dataset is prepared for that*******
+
+# df_test = pd.read_csv("dataset/test.csv")
+# X_train = np.array(df["comment_text"])
+# # print(df_test.head())
+# #print(x_train.shape)
+# y_train = np.array(df[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]])
+# X_test = np.array(df_test["comment_text"])
+# # print(x_test)
+# # print(y_train)
+# l_reg = linear_model.LinearRegression()
+# model = l_reg.fit(X_train, y_train)
+# predictions = model.predict(X_test)
+# print("predictions: ", predictions) 
